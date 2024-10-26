7e574a4a38d30fd4ab1b2032684253b550fadbbe
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-10-26 15:26:16 +0600

    day-2 file created

--- week-1/day-2/sum.cpp
+++ week-1/day-2/sum.cpp
@@ -4,6 +4,9 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout<<"Hello, world!"<<endl;
+    int a = 5;
+    int b = 10;
+    int sum = a+b;
+    cout<<sum<<endl;
     return 0;
 }
